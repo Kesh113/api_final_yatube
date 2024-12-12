a7f3bbc9f666536dc1d8be8059fc0cdc2bd73f9f
kesh113 <kesh193@yandex.ru>
2024-12-12 15:38:07 +0700

    Finish project. Review complete

--- yatube_api/api/permissions.py
+++ yatube_api/api/permissions.py
@@ -3,7 +3,7 @@ from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 class IsAuthor(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return bool(
+        return (
             request.method in SAFE_METHODS
             or request.user
             and request.user == obj.author
